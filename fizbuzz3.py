f9eef0f880404d65fd1e2419091f980928f9e9ba
real-4w <real_4w@protonmail.com>
2021-12-01 17:18:35 +1300

    WIP

--- fizbuzz3.py
+++ fizbuzz3.py
@@ -1,3 +1,3 @@
 def fizbuzz3(number: int):
-    print(', '.join(['fizzbuzz' if x%15 == 0 else 'buzz' if x%5 == 0 else 'fizz' if x%3 == 0 else str(x) for x in range(1,number)]))
+    print(', '.join(['fizzbuzz' if x%15 == 0 else 'buzz' if x%5 == 0 else 'fizz' if x%3 == 0 else str(x) for x in range(1,number+1)]))
 
