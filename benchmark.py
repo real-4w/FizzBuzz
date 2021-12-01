9d7461fb6feec41f088fc8d064343336de122555
real-4w <real_4w@protonmail.com>
2021-12-01 17:17:18 +1300

    WIP

--- benchmark.py
+++ benchmark.py
@@ -1,22 +1,22 @@
 import fizbuzz1 as f1
 import fizbuzz2_oo as f2
 import fizbuzz3 as f3
-import timer
-
 from timer import Timer
+number = 100
+
 t = Timer()
 print("Option 1")
 t.start()
-f1.fizzbuzz(100)
+f1.fizzbuzz(number)
 t.stop()
 
 print("Option 2")
 t.start()
 ob2 = f2.Solution()
-print(ob2.fizzBuzz2(100))
+print(ob2.fizzBuzz2(number))
 t.stop()
 
 print("Option 3")
 t.start()
-f3.fizbuzz3()
+f3.fizbuzz3(number)
 t.stop()
--- fizbuzz3.py
+++ fizbuzz3.py
@@ -1,4 +1,3 @@
-def fizbuzz3():
-    print('\n'.join(['fizzbuzz' if x%15 == 0 else 'buzz' if x%5 == 0 else 'fizz' if x%3 == 0 else str(x) for x in range(1,101)]))
+def fizbuzz3(number: int):
+    print(', '.join(['fizzbuzz' if x%15 == 0 else 'buzz' if x%5 == 0 else 'fizz' if x%3 == 0 else str(x) for x in range(1,number)]))
 
-fizbuzz3()
